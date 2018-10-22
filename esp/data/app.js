f672db79bdd23fbc5b717f9d10dd37bcb6ad9219
xxlukasz11 <xxlukasz11@gmail.com>
2018-10-22 23:09:22 +0200

    add date to requests

--- esp/data/app.js
+++ esp/data/app.js
@@ -3,13 +3,17 @@ var app = angular.module('esp', []);
 app.controller('paramsDisplay', function($scope, $http, $interval){
 	
 	$scope.loadData = function(){
-		$http.get("values.json").then(function(response) {
+		$http({
+            url: "values.json",
+            method: "GET",
+            params: {date: new Date().getTime()}
+        }).then(function(response) {
 			$scope.myData = response.data;
 		});
 	}
 	
 	$scope.resetData = function(){
-		$http.get("http://lukboz.cba.pl/esp/api/reset/").then(function(response) {
+		$http.post("http://lukboz.cba.pl/esp/api/reset/", {date: new Date().getTime()}).then(function(response) {
 			$scope.loadData();
 		}, function(response){
 			$scope.myData = "Cannot reset data";
