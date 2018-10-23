6c2565010f5ff636c529e4538ebeef520cd53c69
robertjankowski <robjanw11@wp.pl>
2018-10-23 13:12:23 +0200

    merging

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,52 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <esp32-hal.h> // temperatureRead function
 
-const char* ssid = "<<WIFI SSID>>";
-const char* password = "<<WIFI PASSWORD>>";
-const char* insertionAddress = "http://lukboz.cba.pl/esp/api/insert/";
+const char *ssid = "Redmi";
+const char *password = "robert000";
+const char *insertionAddress = "http://lukboz.cba.pl/esp/api/insert/";
+int counter = 0;
 
-void setup() {
+void setup()
+{
 	Serial.begin(9600);
 
 	WiFi.begin(ssid, password);
-	while (WiFi.status() != WL_CONNECTED) {
+	while (WiFi.status() != WL_CONNECTED)
+	{
 		delay(500);
 		Serial.println("Connecting to WiFi..");
 	}
 	Serial.println("Connected to the WiFi network");
 }
 
-void loop() {
-	if(WiFi.status() == WL_CONNECTED){
+void loop()
+{
+	if (WiFi.status() == WL_CONNECTED)
+	{
 		HTTPClient http;
 		http.begin(insertionAddress);
 		http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-		String message = "param1=value1&param2=value2";
-
+		// read temperature from buildin sensor and convert to Celcius
+		float currentTemperature = temperatureRead();
+		String message ="param1=" + String(currentTemperature) + "&param2=" + String(counter);
+		
 		int responseCode = http.POST(message);
+	
 		String responseMessage = http.getString();
-
-		if(responseCode > 0)
+		if (responseCode > 0)
+		{	
 			Serial.println("Code: " + String(responseCode) + "; Message: " + responseMessage);
+			Serial.println("Temp: " + String(currentTemperature));
+			++counter;
+		}
 		else
 			Serial.println("Error on sending data");
 
 		http.end();
 	}
-
 	// send request every 3 seconds
 	delay(3000);
 }
