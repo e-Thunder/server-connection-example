d4c97e1762af635e4363b1a89667a12de7f5afc4
robertjankowski <robjanw11@wp.pl>
2018-10-24 22:37:57 +0200

    merging

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <esp32-hal.h> // temperatureRead function
+#include <esp_log.h>   // get timestamp in s
 
 const char *ssid = "Redmi";
 const char *password = "robert000";
 const char *insertionAddress = "http://lukboz.cba.pl/esp/api/insert/";
-int counter = 0;
 
 void setup()
 {
@@ -30,17 +30,17 @@ void loop()
 		http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
 		// read temperature from buildin sensor and convert to Celcius
-		float currentTemperature = temperatureRead();
-		String message ="content={param1:" + String(currentTemperature) + ",param2:" + String(counter) + "}";
-		
+		auto currentTemperature = temperatureRead();
+		// get current timestamp
+		auto timestamp = esp_log_timestamp();
+		String message = "param1=" + String(currentTemperature) + "&param2=" + String(timestamp);
+
 		int responseCode = http.POST(message);
-	
 		String responseMessage = http.getString();
 		if (responseCode > 0)
-		{	
+		{
 			Serial.println("Code: " + String(responseCode) + "; Message: " + responseMessage);
 			Serial.println("Temp: " + String(currentTemperature));
-			++counter;
 		}
 		else
 			Serial.println("Error on sending data");
