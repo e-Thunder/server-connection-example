4a2c49fecf0296294c3aef9feeb2c04a6801511c
robertjankowski <robjanw11@wp.pl>
2018-10-28 15:30:45 +0100

    fix wrong function placement

--- lib/utils_functions/src/utils_functions.cpp
+++ lib/utils_functions/src/utils_functions.cpp
@@ -1,19 +1,2 @@
 #include <Arduino.h>
 #include <utils_functions.h>
-
-template <class T, int N>
-String prepare_message(T (&array)[N])
-{
-	String data = "content={";
-	for (int i = 0; i < N; ++i)
-	{
-		String tmp = "param" + String(i + 1) + ":";
-		data.concat(tmp);
-		data.concat(String(array[i]));
-		if (i < N - 1)
-			data.concat(",");
-		else
-			data.concat("}");
-	}
-	return data;
-}
--- lib/utils_functions/src/utils_functions.h
+++ lib/utils_functions/src/utils_functions.h
@@ -4,6 +4,20 @@
 #include <Arduino.h>
 
 template <class T, int N>
-String prepare_message(T (&array)[N]);
+String prepare_message(T (&array)[N])
+{
+	String data = "content={";
+	for (int i = 0; i < N; ++i)
+	{
+		String tmp = "param" + String(i + 1) + ":";
+		data.concat(tmp);
+		data.concat(String(array[i]));
+		if (i < N - 1)
+			data.concat(",");
+		else
+			data.concat("}");
+	}
+	return data;
+}
 
 #endif // _UTILS_H_
